a416a2d36ff40960a4af59939d3d283e9b9b9fad
Eric Neiva <eric.miranda-neiva@college-de-france.fr>
2024-12-19 19:55:20 +0100

    Plot kymographs and phase diagram

--- test/ActinModel.jl
+++ test/ActinModel.jl
@@ -1,7 +1,8 @@
 using Gridap
 using Random
+using Plots; pythonplot()
 
-function run(L,n,order,Pe,Δt,t₀,T,τᵈkₒ)
+function run(L,n,order,Δt,t₀,T,Pe,τᵈkₒ)
 
   domain = (0,L)
   partition = (n)
@@ -45,7 +46,7 @@ function run(L,n,order,Pe,Δt,t₀,T,τᵈkₒ)
   eₕ = interpolate_everywhere(_eₕ,Uᵐ)
 
   function viscous_flow_problem(Pe,eₕ)
-    β = 0.0001
+    β = 1.0e-1 * Pe
     aᵘ(u,v) = ∫( 2.0 * eₕ * ( ∇(u)⊙∇(v) ) + β * ( u⋅v ) )dΩ
     bᵘ(v) = ∫( Pe * ( ∇(eₕ) ⋅ v ) )dΩ
     aᵘ,bᵘ
@@ -56,7 +57,7 @@ function run(L,n,order,Pe,Δt,t₀,T,τᵈkₒ)
     d(e,ε) = ∫( ∇(e)⋅∇(ε) )dΩ
     c(e,ε) =  ∫( (uₕ⋅∇(e))*ε + tr(∇(uₕ))*(e*ε) )dΩ
     r(e,ε) = ∫( τᵈkₒ*(e*ε) )dΩ
-    γ = 0.001
+    γ = 0.01
     s(e,ε) = ∫( γ*(e*e*e*ε) )dΩ
     l(ε) = ∫( τᵈkₒ*ε )dΩ
     aᵐ(e,ε) = m(e,ε) + c(e,ε) + r(e,ε) + d(e,ε)
@@ -67,6 +68,13 @@ function run(L,n,order,Pe,Δt,t₀,T,τᵈkₒ)
   i = 0
   t = t₀
 
+  nt = 1+Int(T/Δt)
+  nx = n+1
+  kymograph_u = zeros(nt,nx)
+  kymograph_e = zeros(nt,nx)
+  st = range(t₀,T,nt)
+  sx = range(0.0,L,nx)
+
   tol = 1e-8
 
   while t < T + tol
@@ -76,28 +84,69 @@ function run(L,n,order,Pe,Δt,t₀,T,τᵈkₒ)
     aᵘ,bᵘ = viscous_flow_problem(Pe,eₕ)
     opᵘ = AffineFEOperator(aᵘ,bᵘ,Uᵘ,Vᵘ)
     uₕ = solve(opᵘ)
-    
+    vuₓ = uₕ(Point.(sx))
+    kymograph_u[1+i,:] = map(x -> x[1], vuₓ)
+
     aᵐ,bᵐ = transport_problem(uₕ,eₕ,Δt,τᵈkₒ)
     opᵐ = AffineFEOperator(aᵐ,bᵐ,Uᵐ,Vᵐ)
     eₕ = solve(opᵐ)
-   
-    writevtk(Ω,"test_$i",cellfields=["uₕ"=>uₕ,"eₕ"=>eₕ])    
+    kymograph_e[1+i,:] = eₕ(Point.(sx))
 
     i = i + 1
     t = t + Δt
 
   end
 
+  plt = heatmap(sx,st,kymograph_u,c=:jet,yflip=true,yticks=(0.0:0.2:1.0,string.(0.0:0.2:1.0)),size=(600,600))
+  savefig(plt,"plots/actinmodel/u_Pe_$(trunc(Pe,digits=2))_r_$(trunc(τᵈkₒ,digits=2)).png")
+  plt = heatmap(sx,st,kymograph_e,c=:jet,yflip=true,yticks=(0.0:0.2:1.0,string.(0.0:0.2:1.0)),size=(600,600))
+  savefig(plt,"plots/actinmodel/e_Pe_$(trunc(Pe,digits=2))_r_$(trunc(τᵈkₒ,digits=2)).png")
+
+  maxe = maximum(kymograph_e[nt,:])
+  if maxe < 1.0+1.0e-6
+    maxe = 0.9
+  end
+  maximum(kymograph_u[nt,:]),maxe
+
 end
 
-Pe = 1.0
-Δt = 0.001
+Δt = 0.01
 t₀ = 0.0
 T = 1.0
-τᵈkₒ = 1.0
 
 L = 1
-n = 10
+n = 100
 order = 1
 
-run(L,n,order,Pe,Δt,t₀,T,τᵈkₒ)
+# Pe = 200.0
+# τᵈkₒ = 1.0
+# run(L,n,order,Δt,t₀,T,Pe,τᵈkₒ)
+
+pts = 50
+Pes = logrange(10,1000,pts)
+τᵈkₒs = logrange(1,100,pts)
+
+datau = zeros(pts,pts)
+datae = zeros(pts,pts)
+
+for (i,Pe) in enumerate(Pes)
+  for (j,τᵈkₒ) in enumerate(τᵈkₒs)
+    maxu,maxe = run(L,n,order,Δt,t₀,T,Pe,τᵈkₒ)
+    datau[i,j] = maxu
+    datae[i,j] = maxe
+  end
+end
+
+plt = contour(τᵈkₒs,Pes,datae,xscale=:log10,yscale=:log10,
+              size=(600,600),yticks=(10 .^(1:1:3),string.(10 .^(1:1:3))),
+              xticks=(10 .^(0:1:2),string.(10 .^(0:1:2))),
+              xlabel="τᵈkₒ",ylabel="Pe",
+              levels=10, color=:blues, clabels=true, cbar=false, lw=1)
+savefig(plt,"plots/maxe-actomyosinmodel.png")
+
+plt = heatmap(τᵈkₒs,Pes,datau,xscale=:log10,yscale=:log10,
+              size=(600,600),yticks=(10 .^(1:1:3),string.(10 .^(1:1:3))),
+              xticks=(10 .^(0:1:2),string.(10 .^(0:1:2))),
+              xlabel="τᵈkₒ",ylabel="Pe",
+              color=:blues,cbar=true,colorbar_ticks=(0:10:60,string.(0:10:60)))
+savefig(plt,"plots/maxu-actomyosinmodel.png")
--- test/ActomyosinModel.jl
+++ test/ActomyosinModel.jl
@@ -1,7 +1,8 @@
 using Gridap
 using Random
+using Plots; pythonplot()
 
-function run(L,n,order,Pe,Δt,t₀,T,τᵈkₒ)
+function run(L,n,order,Δt,t₀,T,Pe,τᵈkₒ)
 
   domain = (0,L)
   partition = (n)
@@ -45,7 +46,7 @@ function run(L,n,order,Pe,Δt,t₀,T,τᵈkₒ)
   eₕ = interpolate_everywhere(_eₕ,Uᵐ)
 
   function viscous_flow_problem(Pe,eₕ)
-    β = 0.00001
+    β = 1.0e-4 * Pe
     dξ(e) = 4*e / ( e*e + 1 )^2
     aᵘ(u,v) = ∫( 2.0 * ( ∇(u)⊙∇(v) ) + β * ( u⋅v ) )dΩ
     bᵘ(v) = ∫( Pe * ( dξ∘(eₕ) ) * ( ∇(eₕ) ⋅ v ) )dΩ
@@ -66,6 +67,13 @@ function run(L,n,order,Pe,Δt,t₀,T,τᵈkₒ)
   i = 0
   t = t₀
 
+  nt = 1+Int(T/Δt)
+  nx = n+1
+  kymograph_u = zeros(nt,nx)
+  kymograph_e = zeros(nt,nx)
+  st = range(t₀,T,nt)
+  sx = range(0.0,L,nx)
+
   tol = 1e-8
 
   while t < T + tol
@@ -75,28 +83,65 @@ function run(L,n,order,Pe,Δt,t₀,T,τᵈkₒ)
     aᵘ,bᵘ = viscous_flow_problem(Pe,eₕ)
     opᵘ = AffineFEOperator(aᵘ,bᵘ,Uᵘ,Vᵘ)
     uₕ = solve(opᵘ)
-    
+    vuₓ = uₕ(Point.(sx))
+    kymograph_u[1+i,:] = map(x -> x[1], vuₓ)
+
     aᵐ,bᵐ = transport_problem(uₕ,eₕ,Δt,τᵈkₒ)
     opᵐ = AffineFEOperator(aᵐ,bᵐ,Uᵐ,Vᵐ)
     eₕ = solve(opᵐ)
-   
-    writevtk(Ω,"test_$i",cellfields=["uₕ"=>uₕ,"eₕ"=>eₕ])    
+    kymograph_e[1+i,:] = eₕ(Point.(sx))
 
     i = i + 1
     t = t + Δt
 
   end
 
+  plt = heatmap(sx,st,kymograph_u,c=:jet,yflip=true,yticks=(0.0:0.2:1.0,string.(0.0:0.2:1.0)),size=(600,600))
+  savefig(plt,"plots/actomyosinmodel/u_Pe_$(trunc(Pe,digits=2))_r_$(trunc(τᵈkₒ,digits=2)).png")
+  plt = heatmap(sx,st,kymograph_e,c=:jet,yflip=true,yticks=(0.0:0.2:1.0,string.(0.0:0.2:1.0)),size=(600,600))
+  savefig(plt,"plots/actomyosinmodel/e_Pe_$(trunc(Pe,digits=2))_r_$(trunc(τᵈkₒ,digits=2)).png")
+
+  maxe = maximum(kymograph_e[nt,:])
+  if maxe < 1.0+1.0e-6
+    maxe = 0.9
+  end
+  maximum(kymograph_u[nt,:]),maxe
+
 end
 
-Pe = 100.0
 Δt = 0.01
 t₀ = 0.0
 T = 1.0
-τᵈkₒ = 1.0
 
 L = 1
-n = 20
+n = 100
 order = 1
 
-run(L,n,order,Pe,Δt,t₀,T,τᵈkₒ)
+pts = 100
+Pes = logrange(10,1000,pts)
+τᵈkₒs = logrange(1,100,pts)
+
+datau = zeros(pts,pts)
+datae = zeros(pts,pts)
+
+for (i,Pe) in enumerate(Pes)
+  for (j,τᵈkₒ) in enumerate(τᵈkₒs)
+    maxu,maxe = run(L,n,order,Δt,t₀,T,Pe,τᵈkₒ)
+    datau[i,j] = maxu
+    datae[i,j] = maxe
+  end
+end
+
+plt = contour(τᵈkₒs,Pes,datae,xscale=:log10,yscale=:log10,
+              size=(600,600),yticks=(10 .^(1:1:3),string.(10 .^(1:1:3))),
+              xticks=(10 .^(0:1:2),string.(10 .^(0:1:2))),
+              xlabel="τᵈkₒ",ylabel="Pe",
+              levels=10, color=:blues, clabels=true, cbar=false, lw=1)
+savefig(plt,"plots/maxe-actomyosinmodel.png")
+
+plt = heatmap(τᵈkₒs,Pes,datau,xscale=:log10,yscale=:log10,
+              size=(600,600),yticks=(10 .^(1:1:3),string.(10 .^(1:1:3))),
+              xticks=(10 .^(0:1:2),string.(10 .^(0:1:2))),
+              xlabel="τᵈkₒ",ylabel="Pe",
+              color=:blues,cbar=true,colorbar_ticks=(0:10:60,string.(0:10:60)))
+savefig(plt,"plots/maxu-actomyosinmodel.png")
